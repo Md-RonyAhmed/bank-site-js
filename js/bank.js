91ebbeeca577b6bccc5637ce8a49036992d73f81
Md. Rony <rony15-10527@diu.edu.bd>
2022-02-13 03:35:07 +0600

    Error handling added

--- js/bank.js
+++ js/bank.js
@@ -1,21 +1,26 @@
-
 function getInputValue(inputId) {
   const input = document.getElementById(inputId);
   const amount = parseFloat(input.value);
-  input.value = '';
+  input.value = "";
   return amount;
 }
 
-function updateTotalAmount(amountId,totalAmount) {
+function updateTotalAmount(amountId, totalAmount) {
   const amount = document.getElementById(amountId);
   const total = parseFloat(amount.innerText);
   // update deposit amount
   amount.innerText = totalAmount + total;
 }
 
-function updateBalance(amountId,balance,isAdd) {
+function currentBalance(amountId) {
   const balanceText = document.getElementById(amountId);
   const balanceAmount = parseFloat(balanceText.innerText);
+  return balanceAmount;
+}
+
+function updateBalance(amountId, balance, isAdd) {
+  const balanceText = document.getElementById(amountId);
+  const balanceAmount = currentBalance("balance-amount");
   // update balance amount
   if (isAdd === true) {
     balanceText.innerText = balance + balanceAmount;
@@ -24,14 +29,25 @@ function updateBalance(amountId,balance,isAdd) {
   }
 }
 
-document.getElementById('deposit-btn').addEventListener('click', function () {
-  const depositAmount = getInputValue('deposit-input');
-  const totalDeposit = updateTotalAmount('deposit-amount', depositAmount);
-  const totalBalance = updateBalance('balance-amount',depositAmount,true);
+document.getElementById("deposit-btn").addEventListener("click", function () {
+  const depositAmount = getInputValue("deposit-input");
+  if (depositAmount > 0) {
+    const totalDeposit = updateTotalAmount("deposit-amount", depositAmount);
+    const totalBalance = updateBalance("balance-amount", depositAmount, true);
+  } else {
+    alert("Please enter positive value for deposit.");
+  }
 });
 
-document.getElementById('withdraw-btn').addEventListener('click', function () {
-  const withdrawAmount = getInputValue('withdraw-input');
-  const totalDeposit = updateTotalAmount('withdraw-amount', withdrawAmount);
-  const totalBalance = updateBalance('balance-amount', withdrawAmount, false);
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  const withdrawAmount = getInputValue("withdraw-input");
+  const currentAmount = currentBalance("balance-amount");
+  if (withdrawAmount > 0 && withdrawAmount <= currentAmount) {
+    const totalDeposit = updateTotalAmount("withdraw-amount", withdrawAmount);
+    const totalBalance = updateBalance("balance-amount", withdrawAmount, false);
+  } else if (withdrawAmount > currentAmount) {
+    alert("You have no balance left for withdraw please deposit first.");
+  } else {
+    alert("Please enter positive value for withdraw.");
+  }
 });
